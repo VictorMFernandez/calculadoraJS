1c97e776c71484dc5e9a0223f516e6db2820d5ef
VictorMFernandez <manuel1508vc@gmail.com>
2025-08-15 22:49:50 -0500

    avancando proyecto de calculadota

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log("Hello, World!");
+
+let numeroActual = '0';
+let operador = '';
+let operando = '';
+
+const inputDisplay = document.querySelector('.inputDisplay');
+
+//console.log(inputDisplay);
+
+const buttons = document.querySelectorAll('.button');
+
+//console.log(buttons);
+
+buttons.forEach(function(button){
+    //console.log(button);
+    button.addEventListener('click', function(event){
+        console.log('Me hicieron click');
+        console.log('event')
+
+    })        
+})
